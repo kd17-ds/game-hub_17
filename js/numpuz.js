601f46a76ffbbb3d9f12dd05c2f29f2889060d38
Divyansh Sharma <shan17div@gmail.com>
2024-07-28 23:11:00 +0530

    Number puzzle movements done

--- js/numpuz.js
+++ js/numpuz.js
@@ -1,8 +1,9 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, undefined];
 let btns = document.querySelectorAll(".btn");
+let sequence = [];
 
 const shuffledNumbers = numbers.slice().sort(() => Math.random() - 0.5);
-
+console.log(shuffledNumbers);
 function shuffleNumbers() {
   btns.forEach((btn, index) => {
     if (shuffledNumbers[index] !== undefined) {
@@ -17,8 +18,65 @@ shuffleNumbers();
 
 btns.forEach((btn, index) => {
   btn.addEventListener("click", () => {
-    if (btn.innerText !== "") {
-      console.log(btn, index);
+    // Movement for next block
+    if (
+      btn.innerText !== "" &&
+      (index + 1) % 4 !== 0 &&
+      index + 1 < btns.length &&
+      btns[index + 1].innerText === ""
+    ) {
+      btns[index + 1].innerText = btn.innerText;
+      btn.innerText = "";
+    }
+
+    // Movement for below box
+    if (
+      btn.innerText !== "" &&
+      index + 4 < btns.length &&
+      btns[index + 4].innerText === "" &&
+      index <= 11
+    ) {
+      btns[index + 4].innerText = btn.innerText;
+      btn.innerText = "";
+    }
+
+    //Movement for Upper box
+    if (
+      btn.innerText !== "" &&
+      index - 4 >= 0 &&
+      btns[index - 4].innerText === ""
+    ) {
+      btns[index - 4].innerText = btn.innerText;
+      btn.innerText = "";
+    }
+
+    //Movement for back block
+    if (
+      btn.innerText !== "" &&
+      index - 1 >= 0 &&
+      index % 4 !== 0 &&
+      btns[index - 1].innerText === ""
+    ) {
+      btns[index - 1].innerText = btn.innerText;
+      btn.innerText = "";
     }
   });
 });
+
+// function checkAns() {
+//   btns.forEach((btn, index) => {
+//     btn.addEventListener("click", () => {
+//       sequence = [];
+//       btns.forEach((button, i) => {
+//         sequence.push(button.innerText);
+//       });
+//       console.log(sequence);
+//       if (numbers == sequence) {
+//         console.log("Systum");
+//       } else {
+//         console.log("no system");
+//       }
+//     });
+//   });
+// }
+// checkAns();
