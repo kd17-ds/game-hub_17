0b80da1c67cbfb4ab0ef18055b7a3f821faf7748
Divyansh Sharma <shan17div@gmail.com>
2024-08-17 23:39:16 +0530

    Working on memory game

---
+++ js/mg.js
@@ -0,0 +1,51 @@
+const picturesArr = [
+  "img/mg-imgs/monkey.png",
+  "img/mg-imgs/mango.png",
+  "img/mg-imgs/moon.png",
+  "img/mg-imgs/lemon.png",
+  "img/mg-imgs/monkey.png",
+  "img/mg-imgs/lion.png",
+  "img/mg-imgs/snowman.png",
+  "img/mg-imgs/tree.png",
+  "img/mg-imgs/mango.png",
+  "img/mg-imgs/snowman.png",
+  "img/mg-imgs/lion.png",
+  "img/mg-imgs/tree.png",
+  "img/mg-imgs/butterfly.png",
+  "img/mg-imgs/moon.png",
+  "img/mg-imgs/lemon.png",
+  "img/mg-imgs/butterfly.png",
+  "img/mg-imgs/apple.png",
+  "img/mg-imgs/apple.png",
+  "img/mg-imgs/ball.png",
+  "img/mg-imgs/ball.png",
+];
+let shuffledImages = picturesArr.slice().sort(() => Math.random() - 0.5);
+let images = document.querySelectorAll(".mgcard");
+
+function displayImages() {
+  images.forEach((image, index) => {
+    image.innerHTML = `<img
+          src=${shuffledImages[index]}
+          alt=""
+        />`;
+  });
+}
+displayImages();
+
+setTimeout(() => {
+  images.forEach((image, index) => {
+    console.log(image);
+    image.classList.add("animate__flipInY");
+    image.innerHTML = "";
+  });
+}, 3000);
+
+images.forEach((image, index) => {
+  image.addEventListener("click", () => {
+    image.innerHTML = `<img
+    src=${shuffledImages[index]}
+    alt=""
+  />`;
+  });
+});
